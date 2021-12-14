38f796168d3a385ff205b9560e9eac5110a96cb6
Wodey <54207104+Wodey@users.noreply.github.com>
2021-12-14 16:01:31 +0300

    + python code runner + dynamic change of data

--- Smart/taskmenusmart.py
+++ Smart/taskmenusmart.py
@@ -1,10 +1,12 @@
+import imp
+import json
+
 from Dumb.task_menu import Ui_Form as TaskMenu
 from Smart.taskmenuareyousuresmart import TaskMenuAreYouSureSmart as DialogWin
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 
 
-# TODO read name,body, tests of function from the file
 # TODO write code from input to file on the send button
 # TODO add run test button in designer
 # TODO add a way to run a function from the code with test arguments
@@ -13,9 +15,19 @@ class TaskMenuSmart(QtWidgets.QWidget, TaskMenu):
     def __init__(self, widget=None):
         super().__init__()
         self.widget = widget
+        self.recieve_data()
         self.setupUi(self)
         self.pushButton.clicked.connect(self.open_dialog_win)
         self.pushButton_2.clicked.connect(self.goto_page_2)
+        self.pushButton_3.clicked.connect(self.run_code)
+
+        _translate = QtCore.QCoreApplication.translate
+        self.label_6.setText(_translate("Form", f"Для f({self.data['tests'][0]['argument']}): "))
+        self.label_7.setText(_translate("Form", f"Ожидается: {self.data['tests'][0]['value']}"))
+        self.label_9.setText(_translate("Form", f"Для f({self.data['tests'][1]['argument']}): "))
+        self.label_8.setText(_translate("Form", f"Ожидается: {self.data['tests'][1]['value']}"))
+        self.label_10.setText(_translate("Form", f"Для f({self.data['tests'][2]['argument']}): "))
+        self.label_11.setText(_translate("Form", f"Ожидается: {self.data['tests'][2]['value']}"))
 
     def goto_page_2(self):
         self.widget.setCurrentIndex(1)
@@ -26,6 +38,20 @@ class TaskMenuSmart(QtWidgets.QWidget, TaskMenu):
         dialog.exec_()
         dialog.show()
 
+    def recieve_data(self):
+        with open("data.json") as dfile:
+            self.data = json.load(dfile)
+
+    def run_code(self):
+        code = self.textEdit.toPlainText()
+        _translate = QtCore.QCoreApplication.translate
+        with open("code.py","w") as codefile:
+            codefile.writelines(code)
+        import code
+        imp.reload(code)
+        print(code.solution(self.data['tests'][0]['argument']))
+        print(code.solution(self.data['tests'][1]['argument']))
+        print(code.solution(self.data['tests'][2]['argument']))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
